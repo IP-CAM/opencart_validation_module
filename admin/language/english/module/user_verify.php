92a70838af8bdab655ecd8245c500a1854459bf9
Andrey Litvinenko <andreyshade84@gmail.com>
2015-01-21 21:37:40 +0200

    Start

---
+++ admin/language/english/module/user_verify.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 21.01.2015
+ * Time: 13:30
+ */
+
+$_['heading_title']     = 'User Verify';
+$_['text_edit']         = 'User Verify Options';
+$_['text_enabled']      = 'Enable Verify';
+$_['text_disabled']     = 'Disable Verify';
+$_['entry_status']      = 'Verify Status';
+$_['text_success']      = 'Success: You have modified User Verify module!';
+
+
